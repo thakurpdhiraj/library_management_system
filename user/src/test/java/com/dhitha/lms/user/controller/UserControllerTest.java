c0c004e60a2dd4d224d2170723bed5c18f142b29
Dhiraj <dhi37th@gmail.com>
2020-12-26 01:39:10 +0530

    Fix 'user' test cases

--- user/src/test/java/com/dhitha/lms/user/controller/UserControllerTest.java
+++ user/src/test/java/com/dhitha/lms/user/controller/UserControllerTest.java
@@ -11,7 +11,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.dhitha.lms.user.dto.UserDTO;
-import com.dhitha.lms.user.service.UserService;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Collections;
@@ -20,6 +19,7 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -27,29 +27,22 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 /**
- * Tests {@link UserController} Depends on the data.sql file
+ * Tests {@link UserController} Depends on the data-test.sql file
  *
- * <pre>
- * insert into role (id,name) values (null,'USER');
- * insert into role (id,name) values (null,'ADMIN');
- *
- * insert into user (id,name,email,account_non_expired,account_non_locked,created_at,credentials_non_expired,enabled,password,updated_at,username)
- *  values (null,'admin-lms','admin@lms.com',1,1,current_timestamp(),1,1,'$2a$10$8.UnVuG9HHgffUDAlk8qfOuVGkqRzgVymGe07xd00DMxs.AQubh4a',current_timestamp(),'admin');
- *
- * insert into user_role(user_id,role_id) values (1,1);
- * </pre>
  *
  * @author Dhiraj
  */
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@ActiveProfiles("dev")
-public class UserControllerTest {
+@ActiveProfiles("test")
+class UserControllerTest {
 
   private final ObjectMapper objectMapper = new ObjectMapper();
   @Autowired private MockMvc mockMvc;
-  @Autowired private UserService userService;
+
+  @Value("${lms.client.key}")
+  private String apiKey;
 
   @Test
   @Order(0)
@@ -57,15 +50,17 @@ public class UserControllerTest {
 
   @Test
   @Order(1)
-  public void testGetUserNotFound() throws Exception {
-    mockMvc.perform(get("/v1/{id}", 200)).andExpect(status().isNotFound());
+  void testGetUserNotFound() throws Exception {
+    mockMvc
+        .perform(get("/v1/{id}", 200).header("lms-key", apiKey))
+        .andExpect(status().isNotFound());
   }
 
   @Test
   @Order(2)
-  public void testGetUserFound() throws Exception {
+  void testGetUserFound() throws Exception {
     mockMvc
-        .perform(get("/v1/{id}", 1))
+        .perform(get("/v1/{id}", 1).header("lms-key", apiKey))
         .andExpect(status().isOk())
         .andExpect(jsonPath("$.id", is(1)))
         .andExpect(jsonPath("$.username", is("admin")));
@@ -73,11 +68,12 @@ public class UserControllerTest {
 
   @Test
   @Order(3)
-  public void testSaveUserValidationFail() throws Exception {
+  void testSaveUserValidationFail() throws Exception {
     UserDTO mock = UserDTO.builder().username("username").password("password").build();
     mockMvc
         .perform(
             post("/v1")
+                .header("lms-key", apiKey)
                 .content(objectMapper.writeValueAsString(mock))
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .accept(MediaType.APPLICATION_JSON_VALUE))
@@ -86,7 +82,7 @@ public class UserControllerTest {
 
   @Test
   @Order(4)
-  public void testSaveUserOk() throws Exception {
+  void testSaveUserOk() throws Exception {
     objectMapper.disable(MapperFeature.USE_ANNOTATIONS);
     UserDTO mock =
         UserDTO.builder()
@@ -99,6 +95,7 @@ public class UserControllerTest {
     mockMvc
         .perform(
             post("/v1")
+                .header("lms-key", apiKey)
                 .content(objectMapper.writeValueAsString(mock))
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .accept(MediaType.APPLICATION_JSON_VALUE))
@@ -110,11 +107,12 @@ public class UserControllerTest {
 
   @Test
   @Order(5)
-  public void testPutUserNotFound() throws Exception {
+  void testPutUserNotFound() throws Exception {
     UserDTO mock = UserDTO.builder().build();
     mockMvc
         .perform(
             put("/v1/{id}", 200)
+                .header("lms-key", apiKey)
                 .content(objectMapper.writeValueAsString(mock))
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .accept(MediaType.APPLICATION_JSON_VALUE))
@@ -123,11 +121,12 @@ public class UserControllerTest {
 
   @Test
   @Order(6)
-  public void testPutUserOk() throws Exception {
+  void testPutUserOk() throws Exception {
     UserDTO mock = UserDTO.builder().name("user-lms").build();
     mockMvc
         .perform(
             put("/v1/{id}", 2)
+                .header("lms-key", apiKey)
                 .content(objectMapper.writeValueAsString(mock))
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .accept(MediaType.APPLICATION_JSON_VALUE))
@@ -139,21 +138,25 @@ public class UserControllerTest {
 
   @Test
   @Order(7)
-  public void testDeleteUserNotFound() throws Exception {
-    mockMvc.perform(delete("/v1/{id}", 200)).andExpect(status().isNotFound());
+  void testDeleteUserNotFound() throws Exception {
+    mockMvc
+        .perform(delete("/v1/{id}", 200).header("lms-key", apiKey))
+        .andExpect(status().isNotFound());
   }
 
   @Test
   @Order(8)
-  public void testDeleteUserOk() throws Exception {
-    mockMvc.perform(delete("/v1/{id}", 2)).andExpect(status().isNoContent());
+  void testDeleteUserOk() throws Exception {
+    mockMvc
+        .perform(delete("/v1/{id}", 2).header("lms-key", apiKey))
+        .andExpect(status().isNoContent());
   }
 
   @Test
   @Order(9)
-  public void testGetAll() throws Exception {
+  void testGetAll() throws Exception {
     mockMvc
-        .perform(get("/v1"))
+        .perform(get("/v1").header("lms-key", apiKey))
         .andExpect(status().isOk())
         .andExpect(jsonPath("$", hasSize(1)))
         .andExpect(jsonPath("$[0].id", is(1)));
@@ -161,12 +164,13 @@ public class UserControllerTest {
 
   @Test
   @Order(10)
-  public void testGetByCredentials() throws Exception {
+  void testGetByCredentials() throws Exception {
     objectMapper.disable(MapperFeature.USE_ANNOTATIONS);
     UserDTO mock = UserDTO.builder().username("admin").password("pass").build();
     mockMvc
         .perform(
             post("/v1/validate")
+                .header("lms-key", apiKey)
                 .content(objectMapper.writeValueAsString(mock))
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .accept(MediaType.APPLICATION_JSON_VALUE))
@@ -177,12 +181,13 @@ public class UserControllerTest {
 
   @Test
   @Order(11)
-  public void testGetByCredentialsUserNotFound() throws Exception {
+  void testGetByCredentialsUserNotFound() throws Exception {
     objectMapper.disable(MapperFeature.USE_ANNOTATIONS);
     UserDTO mock = UserDTO.builder().username("user").password("pass").build();
     mockMvc
         .perform(
             post("/v1/validate")
+                .header("lms-key", apiKey)
                 .content(objectMapper.writeValueAsString(mock))
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .accept(MediaType.APPLICATION_JSON_VALUE))
@@ -192,16 +197,25 @@ public class UserControllerTest {
 
   @Test
   @Order(11)
-  public void testGetByCredentialsBadRequest() throws Exception {
+  void testGetByCredentialsBadRequest() throws Exception {
     objectMapper.disable(MapperFeature.USE_ANNOTATIONS);
     UserDTO mock = UserDTO.builder().username(null).password("pass").build();
     mockMvc
         .perform(
             post("/v1/validate")
+                .header("lms-key", apiKey)
                 .content(objectMapper.writeValueAsString(mock))
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .accept(MediaType.APPLICATION_JSON_VALUE))
         .andExpect(status().isBadRequest());
     objectMapper.enable(MapperFeature.USE_ANNOTATIONS);
+  }
+
+  @Test
+  @Order(12)
+  void testGetForbidden() throws Exception {
+    mockMvc
+        .perform(get("/v1/{id}", 200))
+        .andExpect(status().isForbidden());
   }
 }
