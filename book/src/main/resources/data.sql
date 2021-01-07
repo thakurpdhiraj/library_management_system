2bb7acdb684a0d113633082e2ee213e1494ee909
Dhiraj <dhi37th@gmail.com>
2021-01-07 20:12:12 +0530

    Adding more test data

--- book/src/main/resources/data.sql
+++ book/src/main/resources/data.sql
@@ -9,3 +9,17 @@ insert into book(id,name,author,publication,category_id,pages,summary,added_at)
   values (null,'Harry Potter 1','JK','WB',2,300,'Summary for Harry Potter 1',current_timestamp());
 insert into book(id,name,author,publication,category_id,pages,summary,added_at)
   values (null,'Harry Potter 2','JK','WB',2,300,'Summary for Harry Potter 2',current_timestamp());
+insert into book(id,name,author,publication,category_id,pages,summary,added_at)
+  values (null,'Harry Potter 3','JK','WB',2,300,'Summary for Harry Potter 3',current_timestamp());
+insert into book(id,name,author,publication,category_id,pages,summary,added_at)
+  values (null,'Harry Potter 4','JK','WB',2,300,'Summary for Harry Potter 4',current_timestamp());
+insert into book(id,name,author,publication,category_id,pages,summary,added_at)
+  values (null,'Harry Potter 5','JK','WB',2,300,'Summary for Harry Potter 5',current_timestamp());
+insert into book(id,name,author,publication,category_id,pages,summary,added_at)
+  values (null,'Harry Potter 6','JK','WB',2,300,'Summary for Harry Potter 6',current_timestamp());
+insert into book(id,name,author,publication,category_id,pages,summary,added_at)
+  values (null,'Harry Potter 7','JK','WB',2,300,'Summary for Harry Potter 7',current_timestamp());
+insert into book(id,name,author,publication,category_id,pages,summary,added_at)
+  values (null,'Vue','DT','DTP',1,300,'Summary for Vue',current_timestamp());
+insert into book(id,name,author,publication,category_id,pages,summary,added_at)
+  values (null,'Spring','DT','DTP',1,300,'Summary for Spring',current_timestamp());
