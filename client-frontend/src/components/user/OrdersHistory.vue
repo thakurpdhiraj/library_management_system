2e0ab0c54fb26742b2d69cf7d0b3502f194d22c5
Dhiraj <dhi37th@gmail.com>
2021-02-07 01:14:37 +0530

    Changed OrderHistory to timeline from table

--- client-frontend/src/components/user/OrdersHistory.vue
+++ client-frontend/src/components/user/OrdersHistory.vue
@@ -5,9 +5,6 @@
     persistent
     overlay-opacity="1"
     :height="$vuetify.breakpoint.mobile ? '100vh' : '50vh'"
-    :footer-props="{
-      'items-per-page-text': 'Orders per page'
-    }"
   >
     <template v-slot:activator="{ on, attrs }">
       <v-btn
@@ -23,40 +20,73 @@
         History
       </v-btn>
     </template>
-    <v-data-table
-      :headers="headers"
-      :items="history"
-      class="elevation-10"
-      :loading="loading"
-      loading-text="Loading...Please wait"
-    >
-      <template v-slot:top>
-        <v-toolbar flat>
-          <v-toolbar-title>My Order History</v-toolbar-title>
-          <v-divider class="mx-5" inset vertical></v-divider>
-          <v-spacer />
-          <v-btn icon @click="close">
-            <v-icon>mdi-close-circle</v-icon>
-          </v-btn>
-        </v-toolbar>
-      </template>
-    </v-data-table>
+    <v-toolbar flat>
+      <v-toolbar-title>My Order History</v-toolbar-title>
+      <v-divider class="mx-5" inset vertical></v-divider>
+      <v-spacer />
+      <v-btn icon @click="close">
+        <v-icon>mdi-close-circle</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-card elevation="0">
+      <v-timeline dense clipped>
+        <v-timeline-item
+          v-for="order in history"
+          :key="order.orderId"
+          class="mb-4"
+          color="green"
+          small
+        >
+          <v-card class="elevation-2">
+            <v-card-title>
+              <span
+                class="green--text mr-5"
+                v-text="order.orderedAt.substring(0, 10)"
+              ></span>
+              {{ order.bookName }}
+            </v-card-title>
+            <v-card-text>
+              <v-row>
+                <v-col cols="12" sm="6" md="3">
+                  <span>Order Id :</span>
+                  <span v-text="order.orderId" class="green--text ml-2"></span>
+                </v-col>
+                <v-col cols="12" sm="6" md="3">
+                  <span>Reference Id :</span>
+                  <span
+                    v-text="order.bookReferenceId"
+                    class="green--text ml-2"
+                  ></span>
+                </v-col>
+                <v-col cols="12" sm="6" md="3">
+                  <span>Collected At :</span>
+                  <span
+                    v-text="order.collectedAt"
+                    class="green--text ml-2"
+                  ></span>
+                </v-col>
+                <v-col cols="12" sm="6" md="3">
+                  <span>Returned At:</span>
+                  <span
+                    v-text="order.returnedAt"
+                    class="green--text ml-2"
+                  ></span>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </v-timeline-item>
+      </v-timeline>
+    </v-card>
   </v-dialog>
 </template>
 
 <script>
-import * as orders from "../../service/order";
+import * as orders from "@/service/order";
 export default {
   data: () => ({
     dialog: false,
     loading: false,
-    headers: [
-      { text: "Book Name", align: "start", value: "bookName" },
-      { text: "Book Reference Id", value: "bookReferenceId", sortable: false },
-      { text: "Ordered At", value: "orderedAt" },
-      { text: "Collected At", value: "collectedAt" },
-      { text: "Returned At", value: "returnedAt" }
-    ],
     history: []
   }),
   methods: {
@@ -64,7 +94,6 @@ export default {
       this.dialog = false;
     },
     findAllOrdersHistory() {
-      console.log("finding orders");
       this.loading = true;
       orders
         .getUsersOrderHistory()
@@ -79,9 +108,6 @@ export default {
           this.$store.commit("setErrorMessage", err.error_description);
         });
     }
-  },
-  created() {
-    console.log("orders history created");
   },
   watch: {
     dialog() {
