7debab3696be20756ec06e9dbfa026675ace3be9
Dhiraj <dhi37th@gmail.com>
2021-04-18 17:08:37 +0530

    Adding docker for FE

---
+++ build.sh
@@ -0,0 +1,7 @@
+cd auth && mvn clean package && cd ..
+cd book && mvn clean package && cd ..
+cd inventory && mvn clean package && cd ..
+cd order && mvn clean package && cd ..
+cd server && mvn clean package && cd ..
+cd user && mvn clean package && cd ..
+cd client-backend && mvn clean package && cd ..
